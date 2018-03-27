109c729bc99cc566100c9a4138bf11f6424af3a9
Ostrikov Maxim <aspixone@gmail.com>
2018-03-27 23:44:51 +0300

    parallax

--- js/fe.js
+++ js/fe.js
@@ -1,3 +1,4 @@
+// homework 1
 function fe1() {
     var c1, c2;
     c1=+prompt('Введите первое число');
@@ -20,5 +21,36 @@ function fe3() {
     dp = a;
     a = b;
     b = dp;
-    alert('Число А= ' + a + '; Число Б= ' + b)
+    alert('Число А= ' + a + '; Число Б= ' + b)}
+// homework 2
+function fe4() {
+    var login = prompt('Введите логин'), truepassword = 'Чёрный Властелин', password;
+    switch (login) {
+        case null:
+        case '':
+            alert('Вход отменен');
+            break;
+        case 'Админ':
+            password = prompt('Введите пароль');
+            if (password === truepassword) {
+                alert('Добро пожаловать!'); break;}
+                else if (password == null){alert('Вход отменен'); break;}
+                else {alert('Пароль неверен'); break;}
+        default:
+            alert('Я вас не знаю');
+    }
+}
+function fe5() {
+    var login,message;
+     login=prompt('Введите логин');
+     (login==='Вася')? message='Привет': (login==='Директор')? message='Здравствуйте': (login==='')? message='нет логина': message='';
+     alert(message);
+}
+function fe6() {
+    var i,a=[];
+
+    for(i=2;i<11;i+=2) {
+     a[a.length]=i;
+    }
+    alert(a);
 }
